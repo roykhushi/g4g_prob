92c6a295cb8e0e9810bc9b91cf87ca2639894edd
Khushi Roy <roykhushi432@gmail.com>
2023-10-06 23:30:25 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Detect Loop in linked list/detect-loop-in-linked-list.java
@@ -0,0 +1,104 @@
+//{ Driver Code Starts
+//Initial template code for JAVA
+
+import java.util.*;
+import java.io.*;
+import java.lang.*;
+
+class Node
+{
+    int data;
+    Node next;
+    
+    Node(int x)
+    {
+        data = x;
+        next = null;
+    }
+}
+
+class GFG
+{
+    public static void makeLoop(Node head, Node tail, int x){
+        if (x == 0) return;
+        
+        Node curr = head;
+        for(int i=1; i<x; i++)
+            curr = curr.next;
+        
+        tail.next = curr;
+    }
+    
+    public static void main (String[] args){
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        
+        while(t--> 0)
+        {
+            int n = sc.nextInt();
+            
+            int num = sc.nextInt();
+            Node head = new Node(num);
+            Node tail = head;
+            
+            for(int i=0; i<n-1; i++)
+            {
+                num = sc.nextInt();
+                tail.next = new Node(num);
+                tail = tail.next;
+            }
+            
+            int pos = sc.nextInt();
+            makeLoop(head, tail, pos);
+            
+            Solution x = new Solution();
+            if( x.detectLoop(head) )
+                System.out.println("True");
+            else
+                System.out.println("False");
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+
+
+//User function template for JAVA
+
+/* Node is defined as
+
+class Node
+{
+    int data;
+    Node next;
+    Node(int d) {data = d; next = null; }
+}
+
+*/
+
+
+class Solution {
+    //Function to check if the linked list has a loop.
+    public static boolean detectLoop(Node head){
+        // Add code here
+        
+        
+        //search the linked list in a unique hashset
+        //if it contains a node return true
+        //else return false
+        //if we are seeing the node for the first time add it in the set
+        HashSet<Node> set = new HashSet<Node>();
+        
+        while(head != null){
+            if(set.contains(head)){
+                return true;
+                
+            }
+            set.add(head);
+            head=head.next;
+        }
+        return false;
+    }
+}
