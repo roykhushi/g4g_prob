1fe64902810bd15430ac9dc4a22ed25250d61f00
Khushi Roy <roykhushi432@gmail.com>
2024-01-18 15:59:42 +0530

    Added Solution - GfG to GitHub

---
+++ Basic/Replace all 0's with 5/replace-all-0s-with-5.java
@@ -0,0 +1,27 @@
+//{ Driver Code Starts
+import java.util.Scanner;
+import java.lang.Math;
+
+class Convert_To_Five {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+        while (T > 0) {
+            int N = sc.nextInt();
+            System.out.println(new GfG().convertfive(N));
+            T--;
+        }
+    }
+}
+// } Driver Code Ends
+
+
+class GfG {
+    int convertfive(int num) {
+        // Your code here
+        String s = Integer.toString(num);
+        s = s.replaceAll("0","5");
+        int newNum = Integer.parseInt(s);
+        return newNum;
+    }
+}
