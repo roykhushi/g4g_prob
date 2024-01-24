51a6d26f1190a9b98f731e7907b136d498bb415c
Khushi Roy <roykhushi432@gmail.com>
2024-01-24 20:21:44 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Chocolate Distribution Problem/chocolate-distribution-problem.java
@@ -0,0 +1,54 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+class GfG
+{
+    public static void main (String[] args)
+    {
+        
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        
+        while(t-- > 0)
+        {
+            int n = sc.nextInt();
+            ArrayList<Integer> arr = new ArrayList<>();
+            for(int i = 0;i<n;i++)
+                {
+                    int x = sc.nextInt();
+                    arr.add(x);
+                }
+            int m = sc.nextInt();
+            
+            Solution ob = new Solution();
+            
+    		System.out.println(ob.findMinDiff(arr,n,m));
+        }
+        
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution
+{
+    public long findMinDiff (ArrayList<Integer> a, int n, int m)
+    {
+        // your code here
+        Collections.sort(a);
+        int first =0;
+        int last = 0;
+        long diff1 = Long.MAX_VALUE;
+        for(int i=0;i<n-m+1;i++){
+            first = a.get(i);
+            last = a.get(i+m-1);
+           int diff = last - first;
+           diff1 = Math.min(diff,diff1); 
+        }
+        return diff1;
+    }
+}
