744db47e0e335adb4e65006ba828d267a4733b37
Khushi Roy <roykhushi432@gmail.com>
2024-01-30 18:09:04 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Multiply two linked lists/multiply-two-linked-lists.java
@@ -0,0 +1,95 @@
+//{ Driver Code Starts
+import java.util.*;
+class Node
+{
+    int data;
+    Node next;
+    Node(int data){
+        this.data=data;
+        next=null;
+    }
+}
+
+class MultiplyTwoLL{
+	public static void main(String[] args){
+		Scanner sc=new Scanner(System.in);
+		int t=sc.nextInt();
+		while(t-->0){
+			Node head=null;
+			Node phead=null;
+			int n=sc.nextInt();
+			while(n-->0){
+				int n1=sc.nextInt();
+				if(head==null)
+				head=new Node(n1);
+			else{
+				addHead(head,n1);
+			}
+			}
+			int m=sc.nextInt();
+			while(m-->0){
+				int n1=sc.nextInt();
+				if(phead==null)
+				phead=new Node(n1);
+			else{
+				addPhead(phead,n1);
+			}
+			}
+		GfG g=new GfG();
+
+System.out.println(g.multiplyTwoLists(head,phead));
+		}
+	}
+    public static void addHead(Node node,int a) {
+          if (node == null)
+            return;
+		if(node.next==null)
+			node.next=new Node(a);
+		else
+			addHead(node.next,a);
+	}
+	public static void addPhead(Node node,int a){
+		 if (node == null)
+            return;
+		if(node.next==null)
+		node.next=new Node(a);
+	else
+		addPhead(node.next,a);
+	}
+	}
+	
+	
+// } Driver Code Ends
+
+
+/*Node is defined as
+class Node
+{
+    int data; 
+    Node next;
+    Node(int data) {
+        this.data=data;
+        this.next = null;
+    }
+}*/
+
+class GfG{
+  /*You are required to complete this method */
+   public long multiplyTwoLists(Node l1,Node l2){
+          //add code here.
+          long mod = 1000000007;
+          long sum1 = 0;
+          long sum2 = 0;
+          Node temp = l1;
+          while(temp != null){//32
+              sum1 = (sum1*10+temp.data)%mod;
+              temp = temp.next;
+          }
+          Node curr = l2;
+          while(curr!=null){
+              sum2 = (sum2*10+curr.data)%mod;
+              curr = curr.next;
+          }
+          return (sum1*sum2)%mod;
+   }
+}
