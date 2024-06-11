86e0d6ebd0f7ff3dd2a91375808c46e1f0998286
Khushi Roy <roykhushi432@gmail.com>
2024-06-11 14:19:43 +0530

    Updated Solution - GfG to GitHub

--- Medium/Nuts and Bolts Problem/nuts-and-bolts-problem.java
+++ Medium/Nuts and Bolts Problem/nuts-and-bolts-problem.java
@@ -42,9 +42,32 @@ public class Main {
 
 class Solution {
     void matchPairs(int n, char nuts[], char bolts[]) {
+        HashMap<Character, Integer> map = new HashMap<>();
+        map.put('!', 1);
+        map.put('#', 2);
+        map.put('$', 3);
+        map.put('%', 4);
+        map.put('&', 5);
+        map.put('*', 6);
+        map.put('?', 7);
+        map.put('@', 8);
+        map.put('^', 9);
+
         
-        Arrays.sort(nuts);
-        Arrays.sort(bolts);
+        Character[] nutsArr = new Character[n];
+        Character[] boltsArr = new Character[n];
+        for (int i = 0; i < n; i++) {
+            nutsArr[i] = nuts[i];
+            boltsArr[i] = bolts[i];
+        }
+
         
-    }
+        Arrays.sort(nutsArr, (a, b) -> map.get(a) - map.get(b));
+        Arrays.sort(boltsArr, (a, b) -> map.get(a) - map.get(b));
+
+        for (int i = 0; i < n; i++) {
+            nuts[i] = nutsArr[i];
+            bolts[i] = boltsArr[i];
+}
+}
 }
