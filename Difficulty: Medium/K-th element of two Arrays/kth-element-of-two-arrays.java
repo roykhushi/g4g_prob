12facbe0fe51ca746eeaff5ffce7987519aa89c4
Khushi Roy <roykhushi432@gmail.com>
2024-08-07 13:50:24 +0530

    Updated Solution - GfG to GitHub

---
+++ Difficulty: Medium/K-th element of two Arrays/kth-element-of-two-arrays.java
@@ -0,0 +1,68 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.io.*;
+import java.lang.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine().trim());
+        while (t-- > 0) {
+            int k = Integer.parseInt(br.readLine().trim());
+
+            String[] line1 = br.readLine().trim().split(" ");
+            int[] arr1 = new int[line1.length];
+            for (int i = 0; i < line1.length; i++) {
+                arr1[i] = Integer.parseInt(line1[i]);
+            }
+
+            String[] line2 = br.readLine().trim().split(" ");
+            int[] arr2 = new int[line2.length];
+            for (int i = 0; i < line2.length; i++) {
+                arr2[i] = Integer.parseInt(line2[i]);
+            }
+
+            Solution ob = new Solution();
+            System.out.println(ob.kthElement(k, arr1, arr2));
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+class Solution {
+    public long kthElement(int k, int arr1[], int arr2[]) {
+        // code here
+        int n = arr1.length;
+        int m = arr2.length;
+        
+        int [] ans = new int[n+m];
+        
+        int i = 0;
+        int j = 0;
+        int x = 0;
+        
+        while(i<n && j<m){
+           if(arr1[i] < arr2[j]){
+               ans[x++] = arr1[i++];
+           }
+           else{
+               ans[x++] = arr2[j++];
+           }
+        }
+        while(i<n){
+            ans[x++] = arr1[i++];
+        }
+        
+        while(j<m){
+            ans[x++] = arr2[j++];
+        }
+        
+        return ans[k-1];
+    }
+}
