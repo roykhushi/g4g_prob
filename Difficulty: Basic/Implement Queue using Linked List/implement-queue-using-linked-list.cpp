7cb6f9d6713acfcc1bb5a773395c96df461ae2f6
Khushi Roy <roykhushi432@gmail.com>
2024-10-19 21:59:39 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Implement Queue using Linked List/implement-queue-using-linked-list.cpp
@@ -0,0 +1,116 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+struct QueueNode
+{
+    int data;
+    QueueNode *next;
+    QueueNode(int a)
+    {
+        data = a;
+        next = NULL;
+    }
+};
+
+struct MyQueue {
+    QueueNode *front;
+    QueueNode *rear;
+    void push(int);
+    int pop();
+    MyQueue() {front = rear = NULL;}
+};
+
+
+
+int main()
+{
+    int T;
+    cin>>T;
+    while(T--)
+    {
+        MyQueue *sq = new MyQueue();
+
+        int Q;
+        cin>>Q;
+        while(Q--){
+        int QueryType=0;
+        cin>>QueryType;
+        if(QueryType==1)
+        {
+            int a;
+            cin>>a;
+            sq->push(a);
+        }else if(QueryType==2){
+            cout<<sq->pop()<<" ";
+
+        }
+        }
+        cout<<endl;
+    }
+    }
+
+// } Driver Code Ends
+
+
+/* Structure of a node in Queue
+struct QueueNode
+{
+    int data;
+    QueueNode *next;
+    QueueNode(int a)
+    {
+        data = a;
+        next = NULL;
+    }
+};
+
+And structure of MyQueue
+struct MyQueue {
+    QueueNode *front;
+    QueueNode *rear;
+    void push(int);
+    int pop();
+    MyQueue() {front = rear = NULL;}
+}; */
+
+//Function to push an element into the queue.
+void MyQueue:: push(int x)
+{
+        // Your Code
+        QueueNode* newNode = new QueueNode(x);
+        if(rear == NULL){
+            newNode->next = rear;//null
+            rear = newNode;
+            front = newNode;
+        }
+        else{
+            rear->next = newNode;
+            rear = newNode;
+        }
+        
+}
+
+//Function to pop front element from the queue.
+int MyQueue :: pop()
+{
+        // Your Code
+        if(front == NULL){
+            return -1;
+        }
+        else{
+            int ans = front->data;
+        
+        QueueNode* temp = front;
+        front = front->next;
+        if(front == NULL){
+            rear = NULL;
+        }
+        delete(temp);
+        
+        
+        return ans;
+        }
+        
+        
+}
