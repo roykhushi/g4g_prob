1d9570329118887039c0e71459b0069073606705
Khushi Roy <roykhushi432@gmail.com>
2024-11-17 23:26:08 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Flattening a Linked List/flattening-a-linked-list.cpp
@@ -0,0 +1,142 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node {
+    int data;
+    struct Node *next;
+    struct Node *bottom;
+
+    Node(int x) {
+        data = x;
+        next = NULL;
+        bottom = NULL;
+    }
+};
+
+void printList(Node *Node) {
+    while (Node != NULL) {
+        printf("%d ", Node->data);
+        Node = Node->bottom;
+    }
+    printf("\n");
+}
+
+
+// } Driver Code Ends
+/* Node structure  used in the program
+
+struct Node{
+    int data;
+    struct Node * next;
+    struct Node * bottom;
+
+    Node(int x){
+        data = x;
+        next = NULL;
+        bottom = NULL;
+    }
+
+};
+*/
+
+class Solution {
+  public:
+  
+    Node* merge(Node* head, Node* mergedHead){
+        Node* dummyNode = new Node(-1);
+        Node* temp = dummyNode;
+        
+        while(head != NULL && mergedHead != NULL){
+            if(head->data < mergedHead->data){
+                temp->bottom = head;
+                temp = head;
+                head = head->bottom;
+            }
+            else{
+                temp->bottom = mergedHead;
+                temp = mergedHead;
+                mergedHead = mergedHead->bottom;
+            }
+            temp->next = NULL;
+        }
+        if(head != NULL){
+            temp->bottom = head;
+        }
+        else{
+            temp->bottom = mergedHead;
+        }
+        if(dummyNode->bottom != NULL){
+            dummyNode->bottom->next = NULL;
+        }
+        return dummyNode->bottom;
+    }
+    
+    // Function which returns the  root of the flattened linked list.
+    Node *flatten(Node *root) {
+        // Your code here
+        if(root == NULL || root->next == NULL){
+            return root;
+        }
+        
+        Node* mergedHead = flatten(root->next);
+        root = merge(root,mergedHead);
+        
+        return root;
+        
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        vector<int> work;
+        string input;
+        getline(cin, input); // Read the entire line for the array elements
+        getline(cin, input); // Read the entire line for the array elements
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            work.push_back(number);
+        }
+        n = work.size();
+
+        Node *head = NULL;
+        Node *pre = NULL;
+
+        for (int i = 0; i < n; i++) {
+            int m = work[i] - 1;
+            int data;
+            cin >> data;
+            Node *temp = new Node(data);
+            if (head == NULL) {
+                head = temp;
+                pre = temp;
+            } else {
+                pre->next = temp;
+                pre = temp;
+            }
+
+            Node *preB = temp;
+            for (int j = 0; j < m; j++) {
+                int temp_data;
+                cin >> temp_data;
+                Node *tempB = new Node(temp_data);
+                preB->bottom = tempB;
+                preB = tempB;
+            }
+        }
+
+        Solution ob;
+        Node *root = ob.flatten(head);
+        printList(root);
+    }
+    return 0;
+}
+
+// } Driver Code Ends
