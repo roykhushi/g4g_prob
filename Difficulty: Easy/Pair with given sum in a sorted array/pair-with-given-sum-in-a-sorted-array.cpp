3c984ff9f8659b70a571361de29a9e8500c4f406
Khushi Roy <roykhushi432@gmail.com>
2025-01-07 01:05:05 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Pair with given sum in a sorted array/pair-with-given-sum-in-a-sorted-array.cpp
@@ -0,0 +1,57 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+class Solution {
+  public:
+    int countPairs(vector<int> &arr, int target) {
+        // Complete the function
+        int n = arr.size();
+        
+        unordered_map<int,int> mp;
+        int count = 0;
+        
+        for(int i=0;i<n;i++){
+            int halfSum = target-arr[i];
+            
+            if(mp.find(halfSum) != mp.end()){
+                count += mp[halfSum];
+            }
+            
+            mp[arr[i]]++;
+        }
+        
+        return count;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+
+    int t;
+    cin >> t;
+    cin.ignore();
+    while (t--) {
+        vector<int> arr;
+        int target;
+        string input;
+        getline(cin, input);
+
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+        cin >> target;
+        cin.ignore();
+        Solution obj;
+        cout << obj.countPairs(arr, target) << endl;
+        cout << "~\n";
+    }
+
+    return 0;
+}
+// } Driver Code Ends
